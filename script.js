68daf9861f91b63f4c413254a0d0af1eee2424a7
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-10 12:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ input1=document.getElementById("counter");
+console.log(input1);
+input2=document.getElementById("incrementBtn");
+function inc(){
+alert(""+input1.textContent);
+    let num=parseInt(input1.textContent);
+    input1.textContent=""+(num+1);
+}
+document.getElementById("incrementBtn").addEventListener("click",inc);
