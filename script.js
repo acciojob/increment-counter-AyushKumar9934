ffabca5975516307882871ed83602b7699f815eb
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-02 16:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-
+let display=document.getElementById("counter");
+let initialval=parseInt(display.innerHTML);
+console.log(display.innerHTML);
+function inc(){
+	alert(initialval);
+	initialval+=1;
+	display.innerHTML=`${initialval}`;
+}
  
